ef098ba2fd2b6edf447eb9ae55b01073c54b0ae0
ayjayy7 <eng.amonijoni@gmail.com>
2019-08-14 18:29:05 +0300

    functions task

---
+++ functions_task.py
@@ -0,0 +1,32 @@
+from datetime import *
+
+today = date.today()
+#date1=date(today.year,today.month,today.day)
+#date2=date(year,month,day)
+
+def check_birthdate(year , month , day):
+	
+	if (year < today.year): 
+				return calculate_age
+	else:
+		return check_birthdate
+
+def calculate_age(year , month , day):
+	year=today.year-int(year)
+	month=today.month-int(month)
+	day=today.day-int(day)
+	print("You are" , year , "years, " , month , "months, and " , day , "days")
+
+
+year=input('Enter year of birth: ')
+month=input('Enter month of birth: ')
+day=input('Enter day of birth: ')
+
+check_birthdate(year , month , day)
+calculate_age(year,month,day)
+
+#check_birthdate(year , month , day)
+#calculate_age(year,month,day)
+
+#print("You are" , year , "years, " , month , "months, and " , day , "days")
+
