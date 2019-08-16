59836c229b347e719aabf7b19df97e278de544a9
ayjayy7 <eng.amonijoni@gmail.com>
2019-08-17 01:57:52 +0300

    Loops task

--- loops_task.py
+++ loops_task.py
@@ -1,9 +1,24 @@
+items=[]
+prices=[]
+quantities=[]
 
+item = input("item (enter done when finished): ")
+items.append(item)
 
-while done != 'done':
-	item=int(input(item (enter "done" when finished): ))
-	price=float(input(price: ))
-	quantity=int(input(quantity: ))
+while item != 'done' :
+	item = input("item (enter done when finished): ")
+	price = input("price: ")
+	quantity = input("quantity: ")
+	#saving the inputs in a dictionary and appending it to the list of items
+	items.append(item)
+	prices.append(price)
+	quantities.append(quantity)
 
+x=0
+tot=0
 
-#receipt
+for x in list:
+	print(quantity[x] , item[x] , tot)
+	tot= tot + price[x]*quantity[x]
+	
+print("------------------ \n total: " , tot)
